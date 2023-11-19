d78304eecf1c2e5054fe66eb40256c325a02120e
vmuijrers <vmuijrers@gmail.com>
2023-11-20 00:28:02 +0100

    Update Rogue.cs

--- BehaviourTreeExample/Assets/Scripts/AI/Agents/Rogue.cs
+++ BehaviourTreeExample/Assets/Scripts/AI/Agents/Rogue.cs
@@ -25,17 +25,4 @@ public class Rogue : MonoBehaviour
     {
         tree?.Run();
     }
-
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.yellow;
-    //    Handles.color = Color.yellow;
-    //    Vector3 endPointLeft = viewTransform.position + (Quaternion.Euler(0, -ViewAngleInDegrees.Value, 0) * viewTransform.transform.forward).normalized * SightRange.Value;
-    //    Vector3 endPointRight = viewTransform.position + (Quaternion.Euler(0, ViewAngleInDegrees.Value, 0) * viewTransform.transform.forward).normalized * SightRange.Value;
-
-    //    Handles.DrawWireArc(viewTransform.position, Vector3.up, Quaternion.Euler(0, -ViewAngleInDegrees.Value, 0) * viewTransform.transform.forward, ViewAngleInDegrees.Value * 2, SightRange.Value);
-    //    Gizmos.DrawLine(viewTransform.position, endPointLeft);
-    //    Gizmos.DrawLine(viewTransform.position, endPointRight);
-
-    //}
 }
