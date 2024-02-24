e9e0a320eb506c4dbe989710e9687d4b271c661e
Michael Yamada <roninnarasu@gmail.com>
2024-02-24 15:22:40 +0100

    btchase missing from previous commit

---
+++ BehaviourTreeExample/Assets/Scripts/AI/BTNodes/BTChase.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BTChase : BTBaseNode
+{
+    private Player player;
+    private NavMeshAgent agent;
+
+    public BTChase(Blackboard _blackboard)
+    {
+        agent = _blackboard.GetVariable<NavMeshAgent>("Agent");
+        player = _blackboard.GetVariable<Player>("Player");
+    }
+    
+    public override TaskStatus Run()
+    {
+        if (Vector3.Distance(agent.transform.position, player.transform.position) < 5.0f)
+        {
+            return TaskStatus.Success;
+        }
+        
+        if (Vector3.Distance(agent.transform.position, player.transform.position) > 10.0f)
+        {
+            return TaskStatus.Failed;
+        }
+        
+        agent.SetDestination(player.transform.position);
+        return TaskStatus.Running;
+    }
+}
